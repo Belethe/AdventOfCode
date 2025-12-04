4dc261a1ef54a7988ef14f0ef914dfebda193cf0
Belethe <alexbelethe@gmail.com>
2025-12-04 22:42:13 +0100

    Initial attempt at AoC 2025

---
+++ 2025/1a.py
@@ -0,0 +1,17 @@
+f = open("input1.txt", "r")
+finalSum = 0
+dial = 50
+for line in f:
+    if line[0] == 'L':
+        dial -= int(line[1:])
+        if dial < 0:
+            dial += 100
+    if line[0] == 'R':
+        dial += int(line[1:])
+        if dial > 99:
+            dial -= 100
+    if dial == 0:
+        finalSum += 1
+  
+f.close()
+print(finalSum)
