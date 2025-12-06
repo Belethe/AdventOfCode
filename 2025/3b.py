5a3ace9316f24f38f0cc055e5278d0debaba3e39
Belethe <alexbelethe@gmail.com>
2025-12-06 01:58:58 +0100

    Working 3b

--- 2025/3b.py
+++ 2025/3b.py
@@ -1,42 +1,44 @@
-f = open("test3.txt", "r")
+def findMax(start, end, string):
+    currentMax = 0
+    currentMaxIndex = 0
+    index = start
+    searchString = string[start:end]
+    if end == 0: ##if end = 0, include rest of string
+        searchString = string[start:]
+    for nStr in searchString:
+        n = int(nStr)
+        if (n > currentMax):
+            currentMax = n
+            currentMaxIndex = index
+        index += 1
+    return (currentMax, currentMaxIndex)
+
+
+f = open("input3.txt", "r")
 sum = 0
 dial = 50
 for line in f:
-    joltage = [0,0,0,0,0,0,0,0,0,0,0,0]
     bank = int(line)
-    i = len(line)
-    for n in str(bank):
-        battery = int(n)
-        if   i > 12 & battery > joltage[0]:
-            joltage = [battery,     0,          0,          0,          0,          0,          0,          0,          0,          0,          0,          0]
-        elif i > 11 & battery > joltage[1]:
-            joltage = [joltage[0],  battery,    0,          0,          0,          0,          0,          0,          0,          0,          0,          0]
-        elif i > 10 & battery > joltage[2]:
-            joltage = [joltage[0],  joltage[1], battery,    0,          0,          0,          0,          0,          0,          0,          0,          0]
-        elif i >  9 & battery > joltage[3]:
-            joltage = [joltage[0],  joltage[1], joltage[2], battery,    0,          0,          0,          0,          0,          0,          0,          0]
-        elif i >  8 & battery > joltage[4]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], battery,    0,          0,          0,          0,          0,          0,          0]
-        elif i >  7 & battery > joltage[5]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], battery,    0,          0,          0,          0,          0,          0]
-        elif i >  6 & battery > joltage[6]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], joltage[5], battery,    0,          0,          0,          0,          0]
-        elif i >  5 & battery > joltage[7]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], joltage[5], joltage[6], battery,    0,          0,          0,          0]
-        elif i >  4 & battery > joltage[8]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], joltage[5], joltage[6], joltage[7], battery,    0,          0,          0]
-        elif i >  3 & battery > joltage[9]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], joltage[5], joltage[6], joltage[7], joltage[8], battery,    0,          0]
-        elif i >  2 & battery > joltage[10]:
-            joltage = [joltage[0],  joltage[1], joltage[2], joltage[3], joltage[4], joltage[5], joltage[6], joltage[7], joltage[8], joltage[9], battery,    0]
-        elif battery > joltage[11]:
-            joltage[11] = battery
-        # print(bank, n, i, joltage)
-        i -= 1
-    joltagestring = str(joltage[0])+str(joltage[1])+str(joltage[2])+str(joltage[3])+str(joltage[4])+str(joltage[5])+str(joltage[6])+str(joltage[7])+str(joltage[8])+str(joltage[9])+str(joltage[10])+str(joltage[11])
-    sum += int(joltagestring)
-    print(joltage)
+    bankStr = str(bank) #No end of line char
+    joltageArray = []
+    currentMax = 0
+    currentMaxIndex = 0
+    index = 0
+
+    joltageArray.append(findMax(0,-11,bankStr)) #Find first ciffer
+
+    i = -10
+    while i <= 0: #Find the rest of the ciffers
+        start = joltageArray[-1][1]+1 #Search in bank starting after the last confirmed ciffer
+        end = i #End in time
+        joltageArray.append(findMax(start,end,bankStr)) #Found ciffer and its index
+        i += 1
+
+    joltage = ""
+    for jolt in joltageArray:
+        joltage += str(jolt[0]) #Get ciffer from tuple
+    sum += int(joltage)
   
 f.close()
 print(sum)
-print(sum==3121910778619) #Test result
+# print(sum==3121910778619) #Test result
